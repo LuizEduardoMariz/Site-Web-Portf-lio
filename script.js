42c6a4253b052c283712b4433bcbaf3ce441032c
Luiz Eduardo de Araújo Mariz <edudearaujomariz@gmail.com>
2025-06-08 23:01:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("bt").addEventListener("click", function () {
+  window.location.href = "projects.html"; 
+});
+document.getElementById("insta").addEventListener("click", function () {
+  window.location.href = "https://www.instagram.com/luizeduardomariz?igsh=cnJod3JsNW8zOTVm"; 
+});
+document.getElementById("git").addEventListener("click", function () {
+  window.location.href = "https://github.com/LuizEduardoMariz"; 
+});
+document.getElementById("link").addEventListener("click", function () {
+  window.location.href = "https://www.linkedin.com/in/luizeduardomariz/?locale=en_US"; 
+});
+document.getElementById("bot").addEventListener("click", function () {
+  window.location.href = "inspirations.html"; 
+});
